c871e8962dd7b3cb899ce4105a27f856acd726d4
Nicholas Major <nick@nmajor.com>
2018-06-08 09:06:47 +0100

    Initial commit with devise template from https://github.com/lewagon/rails-templates

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# db/seeds.rb
+puts 'Creating 5 products...'
+5.times do |i|
+  product = Product.create!(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase
+  )
+  puts "#{i + 1}. #{product.name}"
+end
+puts 'Finished!'
